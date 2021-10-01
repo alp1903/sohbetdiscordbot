f6d63a1168845cae4496520e0596da31ed890312
ALP KIRTILLI <76229088+alp1903@users.noreply.github.com>
2021-10-01 16:48:00 +0300

    Add files via upload

---
+++ Alp bot.py
@@ -0,0 +1,120 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='')
+
+@bot.command()
+async def slm(ctx):
+    await ctx.send('merhaba')
+
+@bot.command()
+async def alp(ctx):
+    await ctx.send('kral adam')
+
+@bot.command()
+async def fexa(ctx):
+    await ctx.send('https://cdn.discordapp.com/attachments/835270111510134804/839063154310643732/836322917301616702.png')
+
+@bot.command()
+async def pip(ctx):
+    await ctx.send("""Sıkça karşılaşılan pip hatalarının çözümü 
+    https://www.youtube.com/watch?v=DXvSOFTnSf0""")
+
+@bot.command()
+async def z3k(ctx):
+    await ctx.send('https://cdn.discordapp.com/attachments/835909317869305867/838859275711414272/824713376084656138.png')
+
+@bot.command()
+async def sezar(ctx):
+    await ctx.send('https://cdn.discordapp.com/attachments/830729833473900544/833717297369710612/Queen_Elizabeth.mp4')
+
+@bot.command()
+async def can(ctx):
+    await ctx.send('https://tenor.com/view/can-yaman-wet-look-abs-gif-14898077')
+
+@bot.command()
+async def serotonin(ctx):
+    await ctx.send('https://tenor.com/view/firststep-chick-snow-gif-4934800')
+
+@bot.command()
+async def ahmet(ctx):
+    await ctx.send('https://cdn.discordapp.com/attachments/835501444488364082/839037087847415818/png-transparent-pencil-drawing-graphy-yellow-pencil-villain-food-photography-color-pencil.png')
+
+@bot.command()
+async def ben(ctx):
+    await ctx.send('adam geldi adam')
+
+@bot.command()
+async def emir(ctx):
+    await ctx.send('Yıkiksin bitmişsin')
+
+@bot.command()
+async def serhat(ctx):
+    await ctx.send('https://tenor.com/view/watermelon-jelly-cat-smile-gif-12045082')
+
+@bot.command()
+async def gül(ctx):
+    await ctx.send('QWQWWQWQWQWWQWQWQWWQ')
+
+@bot.command()
+async def yusuf(ctx):
+    await ctx.send('https://cdn.discordapp.com/attachments/795035717784698952/839065275173175306/-Amazon.jpg')
+
+@bot.command()
+async def salih(ctx):
+    await ctx.send('*p ayyıldız tim yeni rap')
+
+@bot.command()
+async def gül2(ctx):
+    await ctx.send('sjjsjsjsjsjsjsjjsjsjjsjsjsjs')
+
+@bot.command()
+async def gül3(ctx):
+    await ctx.send('hgdasyufdgyusgfyuagsdfyutgYUDSAFGYUIHU')
+
+@bot.command()
+async def yerim(ctx):
+    await ctx.send('bende seni alp abi')
+
+@bot.command()
+async def hi(ctx):
+    await ctx.send('hi')
+
+@bot.command()
+async def probot(ctx):
+    await ctx.send('Ben varken pro bot nedir lan')
+
+@bot.command()
+async def ağla(ctx):
+    await ctx.send('https://tenor.com/view/sad-crying-tears-baby-gif-8822845')
+
+@bot.command()
+async def küçük(ctx):
+    await ctx.send('sensin o')
+
+@bot.command()
+async def hacker(ctx):
+    await ctx.send('https://tenor.com/view/hacker-pc-meme-matrix-codes-gif-16730883')
+
+@bot.command()
+async def çöketme(ctx):
+    await ctx.send('https://thumbs.gfycat.com/SlipperyBelatedKudu-size_restricted.gif')
+
+@bot.command()
+async def davet(ctx):
+    await ctx.send('https://discord.io/soft-sec')
+
+@bot.command()
+async def erkan(ctx):
+    await ctx.send('https://tenor.com/view/por%C3%A7ay-gif-18707400')
+
+
+bot.run('token')
+
+
+
+
+
+
+
+
